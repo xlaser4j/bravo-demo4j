c7c994abb402326cff2aeea8bad5b864596de09d
xlasers <657344389@qq.com>
2018-09-16 23:57:48 +0800

    :sparkles: add tests module

---
+++ tests/src/main/java/com/xlasers/demo/App.java
@@ -0,0 +1,19 @@
+package com.xlasers.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @package: com.xlasers.demo
+ * @author: Elijah.D
+ * @time: CreateAt 2018/9/16 && 23:53
+ * @description:
+ * @copyright: Copyright © 2018 xlasers
+ * @version: V1.0
+ * @modified: Elijah.D
+ */
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        log.info("【... ... ...】");
+    }
+}
