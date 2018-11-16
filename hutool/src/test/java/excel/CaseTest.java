10768597d4ff47a8904da4bfdbe8d7f8f9696277
xlasers <657344389@qq.com>
2018-11-16 17:25:37 +0800

    :white_check_mark: add test case for excel reader

--- hutool/src/test/java/excel/CaseTest.java
+++ hutool/src/test/java/excel/CaseTest.java
@@ -1,12 +1,14 @@
 package excel;
 
+import java.util.List;
+
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.json.JSONUtil;
+import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import cn.hutool.poi.excel.ExcelWriter;
-import com.xlasers.hutool.excel.ColumnInfo;
-import com.xlasers.hutool.excel.DbInfo;
-import com.xlasers.hutool.excel.TableInfo;
-import com.xlasers.hutool.excel.ViewInfo;
+import com.xlasers.hutool.excel.*;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -22,6 +24,7 @@ import org.junit.jupiter.api.Test;
  * @version: V1.0
  * @modified: Elijah.D
  */
+@Slf4j
 public class CaseTest {
     /**
      * 测试,读取beans写入excel
@@ -32,17 +35,42 @@ public class CaseTest {
         ExcelWriter writer = ExcelUtil.getWriter("C:/Users/Solor/Desktop/Code/future/bravo-demos/hutool/demo" + System.currentTimeMillis() + ".xlsx");
 
         writer.setOrCreateSheet("Db");
-        writer.write(CollUtil.newArrayList(new DbInfo())).setRowHeight(-1, 20).setColumnWidth(-1, 15);
+        writer.write(CollUtil.newArrayList(new DbInfoDTO())).setRowHeight(-1, 20).setColumnWidth(-1, 15);
 
         writer.setOrCreateSheet("Table");
-        writer.resetRow().write(CollUtil.newArrayList(new TableInfo())).setRowHeight(-1, 20).setColumnWidth(-1, 15);
+        writer.resetRow().write(CollUtil.newArrayList(new TableInfoDTO())).setRowHeight(-1, 20).setColumnWidth(-1, 15);
 
         writer.setOrCreateSheet("View");
         writer.resetRow().write(CollUtil.newArrayList(new ViewInfo())).setRowHeight(-1, 20).setColumnWidth(-1, 15);
 
         writer.setOrCreateSheet("Column");
-        writer.resetRow().write(CollUtil.newArrayList(new ColumnInfo())).setRowHeight(-1, 20).setColumnWidth(-1, 15);
+        writer.resetRow().write(CollUtil.newArrayList(new ColumnInfoDTO())).setRowHeight(-1, 20).setColumnWidth(-1, 15);
 
         writer.close();
+    }
+
+    /**
+     * 测试,解析excel到bean
+     */
+    @Test
+    public void testReader() {
+
+        List<BaseNeoDTO> data = CollUtil.newArrayList();
+
+        ExcelReader reader = ExcelUtil.getReader("C:/Users/Solor/Desktop/Code/future/bravo-demos/hutool/Import_Model.xlsx");
+        log.info("【获取excel下sheet名字】:{}", reader.getSheetNames());
+
+        List<DbInfoDTO> dbs = reader.setSheet("Db").read(0, 1, DbInfoDTO.class);
+        List<TableInfoDTO> tables = reader.setSheet("Table").read(0, 1, TableInfoDTO.class);
+        List<ViewInfo> views = reader.setSheet("View").read(0, 1, ViewInfo.class);
+        List<ColumnInfoDTO> columns = reader.setSheet("Column").read(0, 1, ColumnInfoDTO.class);
+
+        data.addAll(dbs);
+        data.addAll(tables);
+        data.addAll(views);
+        data.addAll(columns);
+        log.info("【解析成JavaBean】:{}", data);
+
+        log.info("【解析成Json】:{}", JSONUtil.parseArray(data));
     }
 }
