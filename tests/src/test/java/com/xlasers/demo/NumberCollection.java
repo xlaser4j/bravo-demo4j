62dac140759315f053c1ab22df19e01eb408dad5
xlasers <657344389@qq.com>
2019-01-02 17:01:41 +0800

    :construction: work in progress

--- tests/src/test/java/com/xlasers/demo/NumberCollection.java
+++ tests/src/test/java/com/xlasers/demo/NumberCollection.java
@@ -1,5 +1,8 @@
 package com.xlasers.demo;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -32,5 +35,23 @@ public class NumberCollection {
         log.info("【溢出转换】溢出后的值 intValue: {}", (int) value);
 
         assertThrows(ArithmeticException.class, () -> Math.toIntExact(value));
+    }
+
+    /**
+     * Test case 2.
+     *
+     * <p>测试BigDecimal转换百分比
+     */
+    @Test
+    void testCase2() {
+        BigDecimal ratio = BigDecimal.valueOf(0.501);
+
+        DecimalFormat format1 = new DecimalFormat("#%");
+        String s1 = format1.format(ratio);
+        log.info("【s1】:{}", s1);
+
+        DecimalFormat format2 = new DecimalFormat("#.00%");
+        String s2 = format2.format(ratio);
+        log.info("【s2】:{}", s2);
     }
 }
