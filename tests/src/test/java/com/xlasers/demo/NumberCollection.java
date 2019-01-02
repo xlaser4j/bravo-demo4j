efbfe14c4bab2240e746f65e6dd1851afc1a9590
xlasers <657344389@qq.com>
2019-01-02 17:01:41 +0800

    :construction: work in progress

--- tests/src/test/java/com/xlasers/demo/NumberCollection.java
+++ tests/src/test/java/com/xlasers/demo/NumberCollection.java
@@ -1,5 +1,9 @@
 package com.xlasers.demo;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+import cn.hutool.core.util.NumberUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -32,5 +36,38 @@ public class NumberCollection {
         log.info("【溢出转换】溢出后的值 intValue: {}", (int) value);
 
         assertThrows(ArithmeticException.class, () -> Math.toIntExact(value));
+    }
+
+    /**
+     * Test case 2. todo
+     *
+     * <p>测试BigDecimal转换百分比
+     */
+    @Test
+    void testCase2() {
+        BigDecimal ratio1 = BigDecimal.valueOf(0.5051);
+        DecimalFormat format1 = new DecimalFormat("#%");
+        String s1 = format1.format(ratio1);
+        log.info("【s1】:{}", s1);
+
+        DecimalFormat format2 = new DecimalFormat("#.00%");
+        String s2 = format2.format(ratio1);
+        log.info("【s2】:{}", s2);
+
+        BigDecimal ratio2 = BigDecimal.valueOf(0.9355);
+        DecimalFormat format3 = new DecimalFormat("#%");
+        String s3 = format3.format(ratio2);
+        log.info("【s3】:{}", s3);
+
+        DecimalFormat format4 = new DecimalFormat("#.00%");
+        String s4 = format4.format(ratio2);
+        log.info("【s4】:{}", s4);
+
+        BigDecimal ratio = NumberUtil.roundDown(ratio2, 2);
+        log.info("【ratio】:{}", ratio);
+        DecimalFormat format = new DecimalFormat("#%");
+        String s = format.format(ratio);
+        log.info("【s】:{}",s);
+        
     }
 }
