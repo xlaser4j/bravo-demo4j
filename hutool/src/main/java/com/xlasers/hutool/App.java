32b2462701014a907c6f21348ac4caa39b1b1faa
xlasers <657344389@qq.com>
2019-03-08 16:31:56 +0800

    :sparkles: add hutool module

---
+++ hutool/src/main/java/com/xlasers/hutool/App.java
@@ -0,0 +1,23 @@
+package com.xlasers.hutool;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <p>
+ * 主类占位
+ * </p>
+ *
+ * @package: com.xlasers.hutool
+ * @author: Elijah.D
+ * @time: CreateAt 2019/3/8 && 16:11
+ * @description: hutool相关内容集中在测试类
+ * @copyright: Copyright © 2018 xlasers
+ * @version: V1.0
+ * @modified: Elijah.D
+ */
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        log.info("【hutool】:hutool相关测试demo!");
+    }
+}
