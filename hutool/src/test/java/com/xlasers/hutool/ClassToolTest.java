5e380cc665fee2dea5d4784a9699934a0adf96e0
xlasers <657344389@qq.com>
2019-03-08 16:32:51 +0800

    :construction: work in progress

---
+++ hutool/src/test/java/com/xlasers/hutool/ClassToolTest.java
@@ -0,0 +1,17 @@
+package com.xlasers.hutool;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @package: com.xlasers.hutool
+ * @author: Elijah.D
+ * @time: CreateAt 2019/3/8 && 16:24
+ * @description:
+ * @copyright: Copyright © 2018 xlasers
+ * @version: V1.0
+ * @modified: Elijah.D
+ */
+public class ClassToolTest {
+}
