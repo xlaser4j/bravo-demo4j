53c5fbbf04a41208d1312009858186b1d1b13803
Elijah <withxlaser@gmail.com>
2020-02-10 19:13:17 +0800

    :sparkles: add common module for develop

---
+++ common/src/main/java/com/xlaser4j/common/PageResponse.java
@@ -0,0 +1,28 @@
+package com.xlaser4j.common;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.Data;
+
+/**
+ * @package: com.xlaser4j.common
+ * @author: Elijah.D
+ * @time: 2020/2/10 15:30
+ * @description: Page Helper
+ * @modified: Elijah.D
+ */
+@Data
+public class PageResponse<T> implements Serializable {
+    private static final long serialVersionUID = 2119808948386179415L;
+
+    /**
+     * 总记录数
+     */
+    private long total;
+
+    /**
+     * 列表数据
+     */
+    private List<T> list;
+}
